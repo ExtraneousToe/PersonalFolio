2106fd184037258b76222e16d2ef65ad343fe79e
MattCameronLVR <61762139+MattCameronLVR@users.noreply.github.com>
2020-09-14 11:24:26 +1000

    Added basic outline for games listings

---
+++ BlazorApp/Shared/Listing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class Listing
+    {
+        public string Name { get; set; }
+        public string[] Description { get; set; }
+        public string RepositoryLink { get; set; }
+        public string GHPagesLink { get; set; }
+    }
+}
